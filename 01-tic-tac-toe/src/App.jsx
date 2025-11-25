031c5bf8620e47693061ad3e8ac85654a1dc0ef5
demandsupply <giova.zol92@gmail.com>
2025-11-25 13:33:41 +0100

    Add: created new top-level component Game to handly history and moved Board states to it

--- 01-tic-tac-toe/src/App.jsx
+++ 01-tic-tac-toe/src/App.jsx
@@ -5,9 +5,7 @@ function Square({value, onSquareClick}) {
 }
 
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+export function Board({ xIsNext, squares, onPlay}) {
 
   function handleClick(i) {
     if (squares[i]) return;                 // Check if the square already has an 'X' or 'O'
@@ -18,8 +16,7 @@ export default function Board() {
     if (xIsNext) nextSquares[i] = "X";
     else nextSquares[i] = "O";
 
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -50,6 +47,28 @@ export default function Board() {
       </div>
     </>
   )
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );    
 }
 
 
