1bc7ee57a090b12b091e7c21840578d5a735f51a
demandsupply <giova.zol92@gmail.com>
2025-11-25 11:44:33 +0100

    Add: implement game logic to determine winner

--- 01-tic-tac-toe/src/App.jsx
+++ 01-tic-tac-toe/src/App.jsx
@@ -10,7 +10,8 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) return;     // Check if the square already has an 'X' or 'O'
+    if (squares[i]) return;                 // Check if the square already has an 'X' or 'O'
+    if (calculateWinner(squares)) return;   // check if a playes has won
 
     const nextSquares = squares.slice();    // create a copy of the squares array --> IMMUTABLE
 
@@ -20,8 +21,18 @@ export default function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return(
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} /> 
@@ -39,4 +50,26 @@ export default function Board() {
       </div>
     </>
   )
+}
+
+
+function calculateWinner(squares) {
+    const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
